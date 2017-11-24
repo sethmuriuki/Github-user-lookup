81d5a0a9c254e1019ae5405b0053c82572638296
first-name second-name”“git config --global user.email email@example.com” <sethkrm@gmail.com>
2017-11-24 14:43:21 +0300

    add new component search

--- Github-user-lookup/src/app/app.module.ts
+++ Github-user-lookup/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SearchFormComponent } from './search-form/search-form.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchFormComponent
   ],
   imports: [
     BrowserModule
