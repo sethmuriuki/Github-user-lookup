4b0775c8273bdb19452d16fd7946259aadaa7270
first-name second-name”“git config --global user.email email@example.com” <sethkrm@gmail.com>
2017-12-03 18:58:56 +0300

    add ngmodel

--- Github-user-lookup/src/app/app.component.ts
+++ Github-user-lookup/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Http} from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Github Look up';
+
+  constructor(private http: Http) {}
+  
 }
